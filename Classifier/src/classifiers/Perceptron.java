22589779a224ca05c3e6ab2c9af96a10fbec74a2
Kenneth Chiguichon <Kenneth Chiguichon>
2017-03-15 20:34:43 -0400

    Skeleton of functoins for perceptron done

--- Classifier/src/classifiers/Perceptron.java
+++ Classifier/src/classifiers/Perceptron.java
@@ -9,15 +9,21 @@ import preprocessor.Preprocessor.Mode;
 public class Perceptron {
     private int dimension;
     private double[] weightMatrix;
+    private double learningRate;
     private final Mode mode;
-    public static enum Class{
-        POSITIVE,
-        NEGATIVE
+    public static enum Label{
+        POSITIVE(1),
+        NEGATIVE(-1);
+        public final int value;
+        private Label(int value){
+            this.value = value;
+        }
     };
     
-    public Perceptron(int dimension, Mode mode){
+    public Perceptron(int dimension, double learningRate, Mode mode){
         this.dimension = dimension;
         weightMatrix = new double[dimension+1];
+        this.learningRate = learningRate;
         this.mode = mode;
     }
     
@@ -26,17 +32,24 @@ public class Perceptron {
         for(int i = 1; i <= dimension; ++i) weightMatrix[i] = Math.random();
     }
     
-    public void train(double[] vector, Class label){
+    public void train(double[] vector, Label label){
         double output = 0;
         for(int i = 0; i <= dimension; ++i)
             output += vector[i] * weightMatrix[i];
+        updateWeights(vector, squash(output), label);
     }
     
-    public double squash(double input){
-        return 1/(1+Math.exp(-input));
+    private void updateWeights(double[] vector, Label guess, Label label){
+        for(int i = 1; i <= dimension; ++i){
+            weightMatrix[i] = weightMatrix[i-1] + learningRate * (label.value - guess.value) * vector[i];
+        }
     }
     
-    public Class test(double[] vector){
+    public Label squash(double input){
+        return (1/(1+Math.exp(-input)) < .5)? Label.NEGATIVE : Label.POSITIVE;
+    }
+    
+    public Label test(double[] vector){
         return null;
     }
 }
